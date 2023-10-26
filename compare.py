d7e9ef6c4f31c59911370e6dd56e67be6449780e
RKeertishKumar <141417594+RKeertishKumar@users.noreply.github.com>
2023-10-26 17:13:07 +0000

    Learning conditionals now after vscode basics

---
+++ compare.py
@@ -0,0 +1,14 @@
+# Accept values
+
+x = int( input( "What's x? "))
+y = int( input( "What's y? "))
+
+if x > y:
+    print("x is greater than y")
+
+if x < y:
+    print("x is less than y")
+
+if x == y:
+    print("x is equal to y")
+
